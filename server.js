fc0b9b302453a130a109cd6b46c487b4924b0d5c
Marcelo Rodrigues Gonzaga <marcelo.gonzaga@presidencia.gov.br>
2020-10-24 10:52:46 -0300

    Add get all products

--- server.js
+++ server.js
@@ -1,10 +1,14 @@
 const http = require("http");
-const products = required("./data/products.json");
+const products = require("./data/products.json");
 
 const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader("Content-Type", "application/json");
-  res.end(JSON.stringify(products));
+  if (req.url == "/api/products" && req.method === "GET") {
+    res.writeHead(200, { "Content-Type": "application/json" });
+    res.end(JSON.stringify(products));
+  } else {
+    res.writeHead(404, { "Content-Type": "application/json" });
+    res.end(JSON.stringify({ message: "Route not found" }));
+  }
 });
 
 const PORT = process.env.PORT || 5000;
